8815fd6683f463be94c41a4600b2b2b2809e0a96
Akshat Srivastava <srivastava.akshat407@gmail.com>
2025-01-18 02:04:12 +0530

    intial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@bookstore.ym74w.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
